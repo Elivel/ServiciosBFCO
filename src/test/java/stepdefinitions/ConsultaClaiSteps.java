c395179d401104d70f499d758e06a2889f6b8e6a
elivel <elivel88@gmail.com>
2025-03-31 15:26:42 -0500

    feature consultaclai

--- src/test/java/stepdefinitions/ConsultaClaiSteps.java
+++ src/test/java/stepdefinitions/ConsultaClaiSteps.java
@@ -1,32 +1,24 @@
 package stepdefinitions;
-import io.cucumber.java.Before;
 import io.cucumber.java.en.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
 import java.io.File;
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertTrue;
 
 public class ConsultaClaiSteps {
 
     WebDriver driver;
 
-    // Inicializar WebDriver antes de cada escenario
-    @Before
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Automatización\\ServiciosBFCO\\ServiciosBFCO\\src\\test\\resources\\drivers\\chrome.exe");  // Ruta correcta del chromedriver
-        driver = new ChromeDriver();  // Inicializar el driver
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
-
     // Paso 1: El usuario accede a la página de login
     @Given("the user accesses the login page")
     public void the_user_accesses_the_login_page() {
             driver.get("http://f8sc00008/Reports/");  // URL de la página de login
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);  // Espera implícita para cargar los elementos
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);  // Espera implícita para cargar los elementos
         }
 
      //   driver.get("http://f8sc00008/Reports/");  // URL de la página de login
@@ -34,7 +26,7 @@ public class ConsultaClaiSteps {
 
 
     // Paso 2: El usuario ingresa el nombre de usuario y la contraseña
-    @Given("enters the username {string} and the password {string}")
+    @And("enters the username {string} and the password {string}")
     public void enters_the_username_and_the_password(String username, String password) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // timer modal
         //WebElement loginPopup = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Nombre de usuario')]"))); // Esperamos hasta que el texto "Nombre de usuario" esté visible
@@ -58,7 +50,7 @@ public class ConsultaClaiSteps {
     }
 
     // Paso 4: El usuario consulta el reporte
-    @When("queries the report {string}")
+    @And("queries the report {string}")
     public void queries_the_report(String reportName) {
         WebElement carpconciliacion = driver.findElement(By.linkText("Conciliacion_Liquidacion")); //primera carpeta
         carpconciliacion.click();
@@ -74,7 +66,7 @@ public class ConsultaClaiSteps {
     }
 
     // Paso 5: El usuario aplica los parámetros necesarios
-    @When("applies the necessary parameters")
+    @And("applies the necessary parameters")
     public void applies_the_necessary_parameters() {
         WebElement btnGuardar = driver.findElement (By.id("ReportViewerControl_ctl05_ctl04_ctl00_ButtonImg"));
         btnGuardar.click();
@@ -89,13 +81,10 @@ public class ConsultaClaiSteps {
     public void the_report_should_be_downloaded_successfully() {
         // Validación de la descarga (esto puede variar según la configuración de tu navegador)
         File downloadedFile = new File("C:/Downloads/CONSULTA_CLAI.csv");  // Verifica la carpeta de descargas
-        assert downloadedFile.exists();  // Asegúrate de que el archivo se haya descargado
+        //assert downloadedFile.exists();  // Asegúrate de que el archivo se haya descargado
+        assertTrue(downloadedFile.exists());  // Asegúrate de que el archivo haya sido descargado
     }
 
-    // Limpiar recursos después de cada prueba
-   // @After
-   // public void closeBrowser() {
-     //   driver.quit();  // Cerrar el navegador después de ejecutar las pruebas
     }
 
 
