f4594682f1873fd58432105f5261ad4314c8a59e
elivel <elivel88@gmail.com>
2025-04-04 00:24:30 +0200

    chore refactor feature

--- src/test/java/stepdefinitions/ConsultaClaiSteps.java
+++ src/test/java/stepdefinitions/ConsultaClaiSteps.java
@@ -1,41 +1,51 @@
 package stepdefinitions;
-import io.cucumber.java.After;
-import io.cucumber.java.en.*;
-import org.openqa.selenium.*;
 
+import io.cucumber.java.After;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.WebDriver;
+
+@Slf4j
 public class ConsultaClaiSteps {
-   WebDriver driver;
+    WebDriver driver;
+
     // Paso 1: El usuario accede a la página de login
     @Given("the user accesses the login page")
     public void the_user_accesses_the_login_page() throws InterruptedException {
         PageFeatures.pageReportes();
-        }
+    }
 
     // Paso 4: El usuario consulta el reporte
     @Given("queries the directory {string}")
-    public void queries_the_directory (String directoryName) throws InterruptedException {
-        PageFeatures.pagedirectoris(directoryName);
-
+    public void queries_the_directory(String directoryName) throws InterruptedException {
+        PageFeatures.pagedirectories(directoryName);
     }
 
     // Paso 5: El usuario aplica los parámetros necesarios
-    @And("applies the necessary parameters")
-    public void applies_the_necessary_parameters() throws InterruptedException{
-       // PageFeatures.pageDescargaReport();
+    @When("applies the necessary parameters")
+    public void applies_the_necessary_parameters() throws InterruptedException {
+        // PageFeatures.pageDescargaReport();
     }
 
     // Paso 6: El reporte debería ser descargado exitosamente
     @Then("the report should be downloaded successfully")
     public void the_report_should_be_downloaded_successfully() {
-    PageFeatures.pageReportDesktop();
+        PageFeatures.pageReportDesktop();
     }
+
     @Given("queries the report {string}")
     public void queries_the_report(String reportName) throws InterruptedException {
-        PageFeatures.pagedirectoris(reportName);
+        log.info("estoy en el report: {}", reportName);
+        PageFeatures.pagedirectories(reportName);
     }
+
     @And("{string} = {string}")
-    public void add_parameter(String parameterName, String parameterValue){
-    PageFeatures.pagesetParameter(parameterName,parameterValue);
+    public void add_parameter(String parameterName, String parameterValue) {
+        log.info("add_parameter: {} = {}", parameterName, parameterValue);
+        PageFeatures.pagesetParameter(parameterName, parameterValue);
 
     }
 
--- src/test/java/stepdefinitions/PageFeatures.java
+++ src/test/java/stepdefinitions/PageFeatures.java
@@ -1,123 +1,42 @@
 package stepdefinitions;
 
-import io.cucumber.java.After;
-import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
-import io.cucumber.java.BeforeAll;
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
-import java.time.Duration;
 
 import static org.junit.Assert.assertTrue;
 
+@Slf4j
 public class PageFeatures {
     private static WebDriver driver;
 
     @Before
-    public static void pageReportes()throws InterruptedException {
+    public static void pageReportes() throws InterruptedException {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://f8sc00008/Reports");
-        //Thread.sleep(1000);
     }
-    public static void pageConsultaClai()throws InterruptedException{
-        WebElement carpconciliacion = driver.findElement(By.linkText("Conciliacion_Liquidacion")); //primera carpeta
-        carpconciliacion.click();
-        // Hacer scroll hacia abajo
-        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight);");
 
-        // Esperar a que cargue la página (opcional, si se necesita)
-        try {
-            Thread.sleep(2000);  // No es lo más óptimo, mejor usar WebDriverWait
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        WebElement reportconsultacali = driver.findElement(By.xpath("//*[@id=\"main\"]/div/div/section[2]/tiles-view/section/div/div/div/ul/li[25]/report-tile/rs-tile/a[1]")); //reporte
-        reportconsultacali.click();
-        Thread.sleep(5000);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        WebElement nroAutorizacionInput = wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.id("ReportViewerControl_ctl04_ctl05_txtValue")));
-        //WebElement nroAutorizacionInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("nroAutorizacion")));
-        nroAutorizacionInput.click();
-        nroAutorizacionInput.clear();
-        nroAutorizacionInput.sendKeys("399300");
-        nroAutorizacionInput.submit();
+    public static void pagesetParameter(String parameterName, String parameterValue) {
+        WebElement propetiers = driver.findElement(By.cssSelector("data-parametername"));
+        log.info("pageSetParameter: {}", propetiers.toString());
+    }
 
-
-    }
-    public static void pagesetParameter(String parameterName, String parameterValue){
-    WebElement propetiers = driver.findElement(By.cssSelector("data-parametername"));
-        System.out.printf(propetiers.toString());
-    }
-    public static void pageDescargaReport(){
-        WebElement btnGuardar = driver.findElement (By.id("ReportViewerControl_ctl05_ctl04_ctl00_ButtonImg"));
-        btnGuardar.click();
-        WebElement menuDesplegable = driver.findElement(By.className("MenuBarBkGnd"));
-        Select select = new Select (menuDesplegable);
-        select.selectByVisibleText("CSV (delimitado por comas)");
-    }
-    public static void pageReportDesktop (){
+    public static void pageReportDesktop() {
         // Validación de la descarga (esto puede variar según la configuración de tu navegador)
         File downloadedFile = new File("C:/Downloads/CONSULTA_CLAI.csv");  // Verifica la carpeta de descargas
         //assert downloadedFile.exists();  // Asegúrate de que el archivo se haya descargado
         assertTrue(downloadedFile.exists());  // Asegúrate de que el archivo haya sido descargado
     }
 
-    public static void pagedirectoris(String directoryName) throws InterruptedException {
+    public static void pagedirectories(String directoryName) throws InterruptedException {
         WebElement pagedirectoris = driver.findElement(By.linkText(directoryName)); //primera carpeta
         pagedirectoris.click();
-        Thread.sleep(3000);
-
-    }
-    public static void clearingReport() throws InterruptedException {
-        WebElement detalladoEvento = driver.findElement(By.linkText("Daily operation"));
-        detalladoEvento.click();
-        Thread.sleep(2000);
-        WebElement reportClearingPMD = driver.findElement(By.linkText("CLEARING (PMD)"));
-        reportClearingPMD.click();
-        //no encuentra el elemento
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement fecha = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label/span[text()='Fecha Proceso']")));
-       // WebElement fecha = driver.findElement(By.xpath("//label/span[text()='Fecha Proceso']"));
-        fecha.click();
-        fecha.clear();
-    }
-    public static void detalladoMCCAReport() throws InterruptedException {
-        WebElement  Billing = driver.findElement(By.linkText("Billing")); //primera carpeta
-        Billing.click();
-        Thread.sleep(5000);
-        WebElement detalladoEvento = driver.findElement(By.linkText("DETALLADO POR EVENTO MCCA")); //primera carpeta
-        detalladoEvento.click();
-        Thread.sleep(3000);
-        WebElement selectElement = driver.findElement(By.id("ReportViewerControl_ctl04_ctl05_ddValue"));
-        Select select = new Select(selectElement);
-        select.selectByIndex(6);
-
-    }
-    public static void popupLoginDate(){
-        // Paso 2: El usuario ingresa el nombre de usuario y la contraseña
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // timer modal
-        //WebElement loginPopup = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Nombre de usuario')]"))); // Esperamos hasta que el texto "Nombre de usuario" esté visible
-        WebElement usernameField = driver.findElement(By.xpath("//label[contains(text(),'Nombre de usuario')]/following-sibling::input"));
-        WebElement passwordField = driver.findElement(By.xpath("//label[contains(text(),'Contraseña')]/following-sibling::input"));
-        usernameField.sendKeys("elvelasquezl@bancofalabella.com.co");
-        passwordField.sendKeys("Colombia2024@2025.");
-        WebElement loginButton = driver.findElement(By.xpath("//button[contains(text(),'Acceder')]"));
-        loginButton.click();  // Hacer clic en el botón de acceso
-        // passwordField.submit();  // Enviar formulario de login
-        // Opcional: Esperar a que la página cargue después de login, si es necesario.
-        wait.until(ExpectedConditions.urlContains("http://f8sc00008/Reports/browse/"));
-        // valida que se visualice la pagina de reportes
-        WebElement reportPageElement = driver.findElement(By.id("reportsPage"));  // Elemento visible de la página de reportes
-        assert reportPageElement.isDisplayed();
+        //Thread.sleep(3000);
     }
 }
