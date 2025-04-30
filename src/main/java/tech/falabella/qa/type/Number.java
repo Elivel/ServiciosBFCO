9eec6c9b68094712d3db1918c41f64a9e903cda4
elivel <elivel88@gmail.com>
2025-04-30 22:01:17 +0200

    fix number type when is a percentage

--- src/main/java/tech/falabella/qa/type/Number.java
+++ src/main/java/tech/falabella/qa/type/Number.java
@@ -1,31 +1,17 @@
 package tech.falabella.qa.type;
 
-import java.math.BigDecimal;
 import java.util.Optional;
 
-public record Number(Long value) {
+public record Number(Float value) {
 
     public static Number from(String unformatted) {
         return Optional.ofNullable(unformatted)
-                .map(it -> it.replaceAll("[^0-9]", ""))
+                .map(it -> it.replaceAll("[^0-9.]", ""))
                 .map(String::trim)
                 .filter(it -> !it.isEmpty())
-                .map(Long::parseLong)
+                .map(Float::parseFloat)
+                .map(it -> unformatted.contains("%") ? it / 100 : it)
                 .map(Number::new)
                 .orElseGet(() -> new Number(null));
-/*
-        if (unformatted == null || unformatted.isEmpty()) {
-            return new Number(null); // O lanza una excepción, según tu necesidad
-        }
-        try {
-            Long parsedValue = Long.parseLong(unformatted.replaceFirst("^0+(?!$)", ""));
-            return new Number(parsedValue);
-        } catch (NumberFormatException e) {
-            // Maneja la excepción si la cadena no es un número válido
-            System.err.println("Error al convertir la cadena a número: " + unformatted);
-            return null; // O lanza una excepción, según tu necesidad
-        }
-
- */
     }
 }
